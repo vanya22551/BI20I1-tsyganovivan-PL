020e26f52fe2ad9a92ba587ca0715991d146b3b2
vanya22551 <70910324+vanya22551@users.noreply.github.com>
2020-11-13 02:28:37 +0600

    Add files via upload

---
+++ fibinacci_numbers/fibinacci_numbers/fibinacci_numbers.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+
+unsigned long long fibonacci(int n)
+{
+    if (n == 0) { return 0; };
+    if (n == 1 || n == 2) { return 1; };
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    for (int i=0; i < 51; i++) 
+    {
+        std::cout << fibonacci(i); std::cout << " "; std::cout << i << std::endl;
+    }
+    return 0;
+}
