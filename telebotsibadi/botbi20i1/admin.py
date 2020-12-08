0ccb438950adad54051b887402d3092d1c6eaba6
Иван <vanya22551@gmail.com>
2020-12-08 23:06:50 +0600

    Работа в таблице с расчетом рейтинга

--- telebotsibadi/botbi20i1/admin.py
+++ telebotsibadi/botbi20i1/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student, Laboratory, Hint, Stats
+from .models import Student, Laboratory, Hint, Stats, Group
 
 admin.site.register(Hint)
 admin.site.register(Student)
 admin.site.register(Laboratory)
 admin.site.register(Stats)
+admin.site.register(Group)
--- telebotsibadi/botbi20i1/views.py
+++ telebotsibadi/botbi20i1/views.py
@@ -2,15 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
 # Create your views here.
-from .models import Laboratory, Student, Hint, Stats
+from .models import Laboratory, Student, Hint, Stats, Group
 
+def group(request, id):
+    students = Student.objects.filter(group_id__id=id)
 
-def facePage(request):
-    students = Student.objects.all()
 
     stats = Stats.objects.all().order_by('lab')
 
     context = {
+
+        'students': students,
+        'stats': stats,
+    }
+
+    return render(request, template_name='bi-20i1.html', context=context)
+
+
+def facePage(request):
+    students = Student.objects.all()
+    groups = Group.objects.all()
+
+    stats = Stats.objects.all().order_by('lab')
+
+    context = {
+        'groups': groups,
         'students': students,
         'stats': stats,
     }
@@ -46,10 +62,10 @@ def index(request):
 
 
 def table_view(request):
-    students = Student.objects.all()
+    students_bi20i1 = Student.objects.all().filter(group='БИ-20И1')
     stats = Stats.objects.all().order_by('lab')
     context = {
-        'students': students,
+        'students_bi20i1': students_bi20i1,
         'stats': stats,
     }
 
@@ -60,46 +76,57 @@ def update_changes(request):
     if (request.GET):
         stats = Stats.objects.get(pk=request.GET['stats_id'])
         user = Student.objects.get(pk=stats.student.id)
+        if request.GET['status'] == '1':
+            stats.status = True
+
+        else:
+            stats.status = False
         labs_kt1 = user.labs.filter(kt=1)
         labs_kt2 = user.labs.filter(kt=2)
         labs_kt3 = user.labs.filter(kt=3)
-
+        labs_done_kt1 = Stats.objects.filter(student=user, status=True, lab__kt=1)
+        labs_done_kt2 = Stats.objects.filter(student=user, status=True, lab__kt=2)
+        labs_done_kt3 = Stats.objects.filter(student=user, status=True, lab__kt=3)
         KT = stats.lab.kt
 
         sum_labs_1kt = len(labs_kt1)
         sum_labs_2kt = len(labs_kt2)
         sum_labs_3kt = len(labs_kt3)
-
+        update_rating = 100/3
         print(labs_kt1)
         print(stats.lab.kt)
+
         try:
             lab_point_kt1 = 100 / sum_labs_1kt
+            update_rating_point_1kt = update_rating / sum_labs_1kt
         except ZeroDivisionError:
             lab_point_kt1 = 0
+            update_rating_point_1kt = 0
         try:
             lab_point_kt2 = 100 / sum_labs_2kt
+            update_rating_point_2kt = update_rating / sum_labs_2kt
         except ZeroDivisionError:
             lab_point_kt2 = 0
+            update_rating_point_2kt = 0
         try:
             lab_point_kt3 = 100 / sum_labs_3kt
+            update_rating_point_3kt = update_rating / sum_labs_3kt
         except ZeroDivisionError:
             lab_point_kt3 = 0
-        if request.GET['status']:
-            if not stats.status:
-                if KT == 1:
-                    user.rating_1KT = lab_point_kt1
-                elif KT == 2:
-                    user.rating_2KT = lab_point_kt2
-                else:
-                    user.rating_3KT = lab_point_kt3
-            else:
-                user.rating += 0
-
-            stats.status = True
+            update_rating_point_3kt = 0
 
 
+        if KT == 1:
+            user.rating_1KT = lab_point_kt1*len(labs_done_kt1)
+            user.rating = update_rating_point_1kt
+        elif KT == 2:
+            user.rating_2KT = lab_point_kt2*len(labs_done_kt2)
+            user.rating = update_rating_point_2kt
         else:
-            stats.status = False
+            user.rating_3KT = lab_point_kt3*len(labs_done_kt3)
+            user.rating = update_rating_point_3kt
+
+
         user.save()
         stats.save()
 
@@ -108,8 +135,8 @@ def update_changes(request):
             'status': stats.status,
             'user_id': user.id,
             'lab_id': request.GET['stats_id'],
-            'kt_1': user.rating_1KT,
-            'kt_2': user.rating_2KT,
-            'kt_3': user.rating_3KT,
-            'rating': user.rating
+            'kt_1': float('{:.2f}'.format(user.rating_1KT)),
+            'kt_2': float('{:.2f}'.format(user.rating_2KT)),
+            'kt_3': float('{:.2f}'.format(user.rating_3KT)),
+            'rating': float('{:.2f}'.format(user.rating))
         })
